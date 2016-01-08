c26b71819b290fe5d12e81852e4958534f70b8f2
hiken13 <mancraft13@hotmail.es>
2016-01-07 18:15:20 -0600

    Template

--- JS/Controlador.js
+++ JS/Controlador.js
@@ -1,3 +1,14 @@
 angular.module('gisApp',[]).controller('controlador',function($scope,$http){
     $scope.size = 640;
+    $scope.hospitales = Array();
+    $scope.obtenerHospitales = function(){
+      $http({method:'GET',url:'getHospitales.php'}).
+        then(function(response){
+            $scope.hospitales = response;
+            console.log($scope.hospitales); 
+        },function(){
+            console.log("Error obteniendo los hospitales")
+        });
+    };
+    $scope.obtenerHospitales();
 });
